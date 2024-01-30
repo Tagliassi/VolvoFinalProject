1da91c8347fe218ebd1dde49f956073362a58a3a
Tagliassi <rafael.galafassi2005@gmail.com>
2024-01-30 16:31:35 -0300

    First Commit

---
+++ Api/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VolvoFinalProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

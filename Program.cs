a9fafc6ad8d6c9e20d2dc4e117e24052597f6715
Tagliassi <rafael.galafassi2005@gmail.com>
2024-02-04 19:59:07 -0300

    Creating migrations
    Co-authored-by: Jenyffer Laura Prochno Pereira <jenyffer.prochno@pucpr.edu.br>

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using VolvoFinalProject.Api.AutoMappers;
 using VolvoFinalProject.Api.DTOService.Interfaces;
 using VolvoFinalProject.Api.DTOService.Services;
 using VolvoFinalProject.Api.Middlewares;
@@ -13,25 +15,21 @@ using VolvoFinalProject.Api.Repository.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ProjectContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+// Configure services
+ConfigureServices(builder);
 
-// Configuration of Serilog
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console() // Log to the console
-    .WriteTo.File("C:\\VolvoFinalProject\\Api\\log.txt", rollingInterval: RollingInterval.Day) // Log to a file with daily rotation
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .ReadFrom.Configuration(builder.Configuration) // Configuration from appsettings.json
-    .MinimumLevel.ControlledBy(new Serilog.Core.LoggingLevelSwitch
-    {
-        MinimumLevel = GetLogLevel() // Get the configured detail level
-    })
-    .CreateLogger();
+// Configure application
+var app = builder.Build();
+var env = app.Environment;
 
-builder.Logging.ClearProviders(); // Remove default providers
-builder.Logging.AddSerilog(); // Add the Serilog provider
+// Use middleware
+app.UseMiddleware<ErrorMiddleware>();
+
+// Additional configuration based on environment
+ConfigureEnvironment(app, env);
+
+// Run the application
+app.Run();
 
 LogEventLevel GetLogLevel()
 {
@@ -43,48 +41,81 @@ LogEventLevel GetLogLevel()
     return Enum.TryParse<LogEventLevel>(logLevel, out var result) ? result : defaultLogLevel;
 }
 
-// Configure services and application
-builder.Services.AddLogging();
-
-builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
-builder.Services.AddTransient<ICategoryServiceRepository, CategoryServiceRepository>();
-builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
-builder.Services.AddTransient<IBillRepository, BillRepository>();
-builder.Services.AddTransient<IContactsRepository, ContactsRepository>();
-builder.Services.AddTransient<IPartsRepository, PartsRepository>();
-builder.Services.AddTransient<IDealerRepository, DealerRepository>();
-builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
-builder.Services.AddTransient<IVehicleRepository, VehicleRepository>();
-
-builder.Services.AddTransient<ICategoryServiceService, CategoryServiceService>();
-builder.Services.AddTransient<IServiceService, ServiceService>();
-builder.Services.AddTransient<IBillService, BillService>();
-builder.Services.AddTransient<IContactsService, ContactsService>();
-builder.Services.AddTransient<IPartsService, PartsService>();
-builder.Services.AddTransient<IDealerService, DealerService>();
-builder.Services.AddTransient<IEmployeeService, EmployeeService>();
-builder.Services.AddTransient<ICustomerService, CustomerService>();
-builder.Services.AddTransient<IVehicleService, VehicleService>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Configure the application using Startup class
-var app = builder.Build();
-var env = app.Environment;
-
-app.UseMiddleware<ErrorMiddleware>();
-
-if (env.IsDevelopment())
+void ConfigureServices(WebApplicationBuilder builder)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Add controllers and endpoints
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // Application Automapper
+    builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+    // Register DbContext
+    var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<ProjectContext>(options => options.UseSqlServer(connection));
+
+    // Configuration of Serilog
+    ConfigureSerilog(builder);
+
+    // Configure services
+    RegisterRepositories(builder);
+    RegisterServices(builder);
 }
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+void ConfigureSerilog(WebApplicationBuilder builder)
+{
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File("C:\\VolvoFinalProject\\Api\\log.txt", rollingInterval: RollingInterval.Day)
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .ReadFrom.Configuration(builder.Configuration)
+        .MinimumLevel.ControlledBy(new Serilog.Core.LoggingLevelSwitch
+        {
+            MinimumLevel = GetLogLevel()
+        })
+        .CreateLogger();
 
-app.Run();
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog();
+}
+
+void RegisterRepositories(WebApplicationBuilder builder)
+{
+    builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+    builder.Services.AddTransient<ICategoryServiceRepository, CategoryServiceRepository>();
+    builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
+    builder.Services.AddTransient<IBillRepository, BillRepository>();
+    builder.Services.AddTransient<IContactsRepository, ContactsRepository>();
+    builder.Services.AddTransient<IPartsRepository, PartsRepository>();
+    builder.Services.AddTransient<IDealerRepository, DealerRepository>();
+    builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+    builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+    builder.Services.AddTransient<IVehicleRepository, VehicleRepository>();
+}
+
+void RegisterServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddTransient<ICategoryServiceService, CategoryServiceService>();
+    builder.Services.AddTransient<IServiceService, ServiceService>();
+    builder.Services.AddTransient<IBillService, BillService>();
+    builder.Services.AddTransient<IContactsService, ContactsService>();
+    builder.Services.AddTransient<IPartsService, PartsService>();
+    builder.Services.AddTransient<IDealerService, DealerService>();
+    builder.Services.AddTransient<IEmployeeService, EmployeeService>();
+    builder.Services.AddTransient<ICustomerService, CustomerService>();
+    builder.Services.AddTransient<IVehicleService, VehicleService>();
+}
+
+void ConfigureEnvironment(WebApplication app, IHostEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+}
--- obj/Debug/net8.0/VolvoFinalProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/VolvoFinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VolvoFinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f74ea421e5adbdb88bb975ca561b0e280ae4e35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfaf35c2366c79d9529a9857a65ab79e4cf18fa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("VolvoFinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VolvoFinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
